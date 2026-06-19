6aeafab77600a48f4f248b3ab2afd820d46a5719
B0sto <bostoganashvilinika2019@gmail.com>
2026-06-19 20:45:40 +0400

    created responsive navbar and deleted unneccessary files

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
+import Navbar from "./components/icons/Navbar"
 
 function App() {
-
   return (
-    <div>
-      TvShow
+    <div className="p-0 sm:p-4 md:p-6 lg:p-8 bg-[#10141E] flex flex-col lg:flex-row gap-8">
+      <Navbar />
+
+      <div>
+
+      </div>
     </div>
   )
 }
