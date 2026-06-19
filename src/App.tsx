51f4e2bca2b4924bddd3b1e9b95336d8007fdcbb
B0sto <bostoganashvilinika2019@gmail.com>
2026-06-19 21:31:23 +0400

    created responsive searchbar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
-import Navbar from "./components/icons/Navbar"
+import Navbar from "./components/common/Navbar"
+import SearchBar from "./components/common/SearchBar"
 
 function App() {
   return (
-    <div className="p-0 sm:p-4 md:p-6 lg:p-8 bg-[#10141E] flex flex-col lg:flex-row gap-8">
+    <div className="p-0 sm:p-4 md:p-6 lg:p-8 bg-[#10141E] flex flex-col lg:flex-row gap-y-6 md:gap-y-8 gap-x-9">
       <Navbar />
 
-      <div>
-
-      </div>
+      <main className="lg:mt-5">
+        <SearchBar />
+      </main>
     </div>
   )
 }
